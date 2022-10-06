429054ad851cc97f44001b1b097b138db8059b3d
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2022-10-06 04:16:48 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -16,13 +16,9 @@ class Job(models.Model):
     Vacancy=models.IntegerField(default=1)
     salary=models.IntegerField(default=0)
     experince=models.IntegerField(default=1)
-    category=models.ForeignKey('Category',on_delete=models.CASCADE) 
     def __str__(self): 
         return self.title
-    
-class Category(models.Model):
-    name=models.CharField(max_length=50)
-    
+
     
     def __str__(self):
         return self.nameS
